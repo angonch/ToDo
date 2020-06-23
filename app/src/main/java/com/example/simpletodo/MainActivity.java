58e8868b3817fb9199fb22b9293d0115d1fa24cb
Anna Goncharenko <agoncharenko@fb.com>
2020-06-23 15:23:43 -0700

    Made itemsAdapter - add and remove items

--- app/src/main/java/com/example/simpletodo/MainActivity.java
+++ app/src/main/java/com/example/simpletodo/MainActivity.java
@@ -1,13 +1,62 @@
 package com.example.simpletodo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    List<String> items;
+
+    Button btnAdd;
+    EditText etItem;
+    RecyclerView rvItems;
+    ItemsAdapter itemsAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnAdd = findViewById(R.id.btnAdd);
+        etItem = findViewById(R.id.etItem);
+        rvItems = findViewById(R.id.rvItems);
+
+        items = new ArrayList<>();
+        ItemsAdapter.OnLongClickLister onLongClickLister = new ItemsAdapter.OnLongClickLister() {
+            @Override
+            public void onItemLongClicked(int position) {
+                // Delete item from the model
+                items.remove(position);
+                // Notify adapter
+                itemsAdapter.notifyItemRemoved(position);
+                Toast.makeText(getApplicationContext(), "Item removed", Toast.LENGTH_SHORT);
+            }
+        };
+        itemsAdapter = new ItemsAdapter(items, onLongClickLister);
+        rvItems.setAdapter(itemsAdapter);
+        rvItems.setLayoutManager(new LinearLayoutManager(this));
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String todoItem = etItem.getText().toString();
+                // Add item to model
+                items.add(todoItem);
+                // Notify adapter that an item is inserted
+                itemsAdapter.notifyItemInserted(items.size() - 1);
+                etItem.setText("");
+                // Notify user
+                Toast.makeText(getApplicationContext(), "Item added", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
