14c5e4eef86ee24d483b2ea9cfa3d1afc9684678
Anna Goncharenko <agoncharenko@fb.com>
2020-06-23 15:57:02 -0700

    close edit activity when clicked save

--- app/src/main/java/com/example/simpletodo/EditActivity.java
+++ app/src/main/java/com/example/simpletodo/EditActivity.java
@@ -1,7 +1,9 @@
 package com.example.simpletodo;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -19,5 +21,23 @@ public class EditActivity extends AppCompatActivity {
         btnSave = findViewById(R.id.btnSave);
 
         getSupportActionBar().setTitle("Edit Item");
+
+        etItem.setText(getIntent().getStringExtra(MainActivity.KEY_ITEM_TEXT));
+
+        // Save button clicked when user is done editing the item
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create intent which will contain results
+                Intent i = new Intent();
+                // Pass results of editing
+                i.putExtra(MainActivity.KEY_ITEM_TEXT, etItem.getText().toString());
+                i.putExtra(MainActivity.KEY_ITEM_POSITION, getIntent().getExtras().getInt(MainActivity.KEY_ITEM_POSITION));
+                // Set results of intent
+                setResult(RESULT_OK, i);
+                // Finish activity - close screen and return to main
+                finish();
+            }
+        });
     }
 }
